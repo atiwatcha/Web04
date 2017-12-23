d752aa67cf183771476b6eb45152c5f9b8ab8ab4
admin <atiwat.chananet@gmail.com>
2017-12-23 11:20:55 +0700

    Add project files.

---
+++ Web04/Models/Album.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web04.Models
+{
+    public class Album
+    {
+        public Album()
+        {
+            Songs = new HashSet<Song>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        public string Artist { get; set; }
+
+        public string CoverImageUrl { get; set; }
+
+        public ICollection<Song> Songs { get; set; }
+    }
+}
---
+++ Web04/Models/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web04.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        public TimeSpan Length { get; set; }
+    }
+}
