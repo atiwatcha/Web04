6fae027634e42757357561ca360a22b13359f95a
admin <atiwat.chananet@gmail.com>
2017-12-23 14:28:38 +0700

    viewComponent

---
+++ Web04/ViewComponents/SongListViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web04.Data;
+using Web04.Models;
+
+namespace Web04.ViewComponents
+{
+    public class SongListViewComponent : ViewComponent
+    {
+
+        public SongListViewComponent(ApplicationDbContext db)
+        {
+            Db = db;
+        }
+
+        public ApplicationDbContext Db { get; }
+
+        public async Task<IViewComponentResult> InvokeAsync(int albumId)
+        {
+            var album = Db.Albums.Find(albumId);
+            await Db.Entry(album).Collection(x => x.Songs).LoadAsync();
+            // await Db.Entry(album).Reference(x => x.SongWriter).LoadAsync();
+
+            return View(album.Songs.ToList());
+        }
+
+    }
+}
