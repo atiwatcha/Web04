da694c0917754654aa82723618ed7fe5c83a1f99
admin <atiwat.chananet@gmail.com>
2017-12-24 12:02:21 +0700

    playlist

---
+++ Web04/Controllers/PlaylistsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web04.Controllers
+{
+    public class PlaylistsController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web04/ViewComponents/PlaylistListViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web04.Data;
+using Web04.Models;
+
+namespace Web04.ViewComponents
+{
+    public class PlaylistListViewComponent:ViewComponent
+    {
+        private readonly ApplicationDbContext db;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public PlaylistListViewComponent(ApplicationDbContext db,UserManager<ApplicationUser> userManager)
+        {
+            this.db = db;
+            this.userManager = userManager;
+
+
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(){
+            var currentUser = await userManager.FindByEmailAsync(User.Identity.Name);
+
+            var q = from p in db.Playlist
+                    where p.Owner == currentUser
+                    select p;
+
+            return View(await q.ToListAsync());
+        }
+    }
+}
